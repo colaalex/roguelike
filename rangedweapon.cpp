02b2b89ff85bdecfba08419b536cc55357841700
Alex <colaalex4r@mail.ru>
2018-12-20 00:28:31 +0300

    add basic ranged weapon

---
+++ rangedweapon.cpp
@@ -0,0 +1,6 @@
+#include "rangedweapon.h"
+
+RangedWeapon::RangedWeapon()
+{
+
+}
---
+++ rangedweapon.h
@@ -0,0 +1,11 @@
+#ifndef RANGEDWEAPON_H
+#define RANGEDWEAPON_H
+
+
+class RangedWeapon
+{
+public:
+    RangedWeapon();
+};
+
+#endif // RANGEDWEAPON_H
